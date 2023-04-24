4b99e540e7f9c169541e5690017748e161f4ae44
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-04-24 17:44:23 +0300

    Lazy Loading has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Home from ".//components/Home"
-import AboutUs from ".//components/AboutUs"
+//import AboutUs from ".//components/AboutUs"
 import Navbar from './components/Navbar'
 import Mission from './components/Mission'
 import WrongPath from './components/WrongPath'
@@ -10,6 +10,9 @@ import Team from ".//components/Team"
 import Company from ".//components/Company"
 import Members from './components/Members'
 import MemberDetail from './components/MemberDetail'
+import React from 'react'
+
+const LazyAboutUs = React.lazy(() => import(".//components/AboutUs"))
 
 
 function App() {
@@ -19,11 +22,15 @@ function App() {
       <Navbar />
       <Routes >
         <Route path='/' element={<Home />} />  {/* Bu şekilde direk localhost da çalışır */}
-        <Route path='/aboutUs' element={<AboutUs />} />  {/* Bu şekilde url'yi belirtiriz */}
-        <Route path='/mission' element={<Mission />} />
-        <Route path='/history' element={<History/>}>
-          <Route path='company' element={<Company/>}/>  {/* Bunun gibi child path lerin başına / koyunca hata veriyor*/}
-          <Route path='team' element={<Team/>}/>
+        <Route path='/aboutUs' element={
+          <React.Suspense>
+            <LazyAboutUs />
+          </React.Suspense>
+        } />  
+        <Route path='/mission' element={<Mission />} /> {/* Bu şekilde url'yi belirtiriz */}
+        <Route path='/history' element={<History />}>
+          <Route path='company' element={<Company />} />  {/* Bunun gibi child path lerin başına / koyunca hata veriyor*/}
+          <Route path='team' element={<Team />} />
         </Route>
         <Route path='/members' element={<Members />} />
         <Route path='/members/:memberID/:name' element={<MemberDetail />} />
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <>
+      <div>AboutUs</div>
+      <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae adipisci nam eveniet maxime veritatis repudiandae obcaecati quas, facilis, delectus blanditiis omnis magnam vero perferendis aspernatur tempora eos dignissimos recusandae doloribus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae at, temporibus laboriosam, explicabo sequi est debitis perspiciatis cum ab totam neque delectus dolor reprehenderit unde nostrum, id incidunt ea?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae at, temporibus laboriosam, explicabo sequi est debitis perspiciatis cum ab totam neque delectus dolor reprehenderit unde nostrum, id incidunt ea?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae at, temporibus laboriosam, explicabo sequi est debitis perspiciatis cum ab totam neque delectus dolor reprehenderit unde nostrum, id incidunt ea?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae at, temporibus laboriosam, explicabo sequi est debitis perspiciatis cum ab totam neque delectus dolor reprehenderit unde nostrum, id incidunt ea?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae at, temporibus laboriosam, explicabo sequi est debitis perspiciatis cum ab totam neque delectus dolor reprehenderit unde nostrum, id incidunt ea?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae at, temporibus laboriosam, explicabo sequi est debitis perspiciatis cum ab totam neque delectus dolor reprehenderit unde nostrum, id incidunt ea?
+      </div>
+    </>
   )
 }
