29961aa6a61ac260007e5bc7766e0a55b4865257
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-04-24 15:43:18 +0300

    Link has been added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Home from ".//components/Home"
 import AboutUs from ".//components/AboutUs"
+import Navbar from './components/Navbar'
+
 
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       <Routes >
-        <Route path='/' element={<Home/>} />  {/* Bu şekilde direk localhost da çalışır */} 
-        <Route path='/aboutUs' element={<AboutUs/>} />  {/* Bu şekilde url'yi belirtiriz */}
+        <Route path='/' element={<Home />} />  {/* Bu şekilde direk localhost da çalışır */}
+        <Route path='/aboutUs' element={<AboutUs />} />  {/* Bu şekilde url'yi belirtiriz */}
       </Routes>
     </>
   )
