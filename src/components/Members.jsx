35d5b3bec27e57e3e9dbd282d92fe30c0b182928
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-04-24 17:33:38 +0300

    UseSearchParams has been added

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useSearchParams } from "react-router-dom"
 
 function Members() {
-  return (
-    <div>Members</div>
-  )
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const isActive = searchParams.get("filter") === "active";  {/* Filtre active ise değişken true, yoksa false olur */}
+
+    return (
+        <>
+            <div>Members</div>
+            <button onClick={() => setSearchParams({filter: "active"})}>Aktif üyeler</button>
+            <button onClick={() => setSearchParams()}>Tüm üyeler</button>
+            {isActive ? <h2>Aktif Üyeler</h2> : <h2>Tüm üyeler</h2>}
+        </>
+    )
 }
 
 export default Members
