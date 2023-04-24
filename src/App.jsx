a9dbb0c5bb7cc0b373683f6e8fe863cd33d76210
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-04-24 16:52:43 +0300

    Nested Routers have been added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ import AboutUs from ".//components/AboutUs"
 import Navbar from './components/Navbar'
 import Mission from './components/Mission'
 import WrongPath from './components/WrongPath'
+import History from './components/History'
+import Team from ".//components/Team"
+import Company from ".//components/Company"
+
 
 function App() {
 
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       <Routes >
         <Route path='/' element={<Home />} />  {/* Bu şekilde direk localhost da çalışır */}
         <Route path='/aboutUs' element={<AboutUs />} />  {/* Bu şekilde url'yi belirtiriz */}
         <Route path='/mission' element={<Mission />} />
+        <Route path='/history' element={<History/>}>
+          <Route path='company' element={<Company/>}/>  {/* Bunun gibi child path lerin başına / koyunca hata veriyor*/}
+          <Route path='team' element={<Team/>}/>
+        </Route>
         <Route path='*' element={<WrongPath />} />  {/* Yukarıdaki url'ler harici bu sayfaya gider */}
       </Routes>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,10 @@ import { Link, NavLink } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className='mainNav'>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/aboutUs"}>About Us</NavLink>
+        <NavLink to={"/history"}>History</NavLink>
 
     </nav>
   )
